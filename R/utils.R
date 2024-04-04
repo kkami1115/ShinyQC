3dd6cf2fcadf07606e134774c4069b4ee4e9fdc6
kkami1115 <35270465+kkami1115@users.noreply.github.com>
2024-04-05 00:19:20 +0900

    added utils R scripts

---
+++ R/utils.R
@@ -0,0 +1,16 @@
+#' @noRd
+dummy <- function() {
+  dplyr::across
+  magrittr::add
+  Rfastp::qcSummary
+  shiny::a
+  tibble::add_case
+  progressr::handler_ascii_alert
+  shinyjs::addClass
+  bslib::accordion
+  fs::as_fs_bytes
+  furrr::furrr_options
+  future::as.cluster
+  plotly::add_annotations
+  shinyFiles::getVolumes
+}
